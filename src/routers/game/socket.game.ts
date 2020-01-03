977b627367699829b41e70188aa5b167219b01a7
Andy0414 <pjh8667@naver.com>
2020-01-03 16:12:22 +0900

    UPDATE multi play

--- src/routers/game/socket.game.ts
+++ src/routers/game/socket.game.ts
@@ -1,5 +1,7 @@
 import { SocketRouter } from "../socket.index";
-import { Socket } from "socket.io";
+
+type JoinRoomRequest = { roomName: string };
+type PropRequest = { roomName: string; prop: any };
 
 class Room {
 	public roomName: string;
@@ -17,19 +19,31 @@ class Room {
 		this.users.delete(socketId);
 		return this.users.size <= 0;
 	}
+
+	addProp(prop: any) {
+		this.props.push(prop);
+	}
+	updateProp(prop: any) {
+		let propIdx = this.props.findIndex((_prop: any) => _prop._id == prop._id);
+		this.props[propIdx] = prop;
+	}
+	deleteProp(prop: any) {
+		let propIdx = this.props.findIndex((_prop: any) => _prop._id == prop._id);
+		this.props.splice(propIdx, 1);
+	}
 }
 
 class RoomManager {
-	static roomManager = new RoomManager();
-	public rooms: Room[] = [];
+	private static roomManager = new RoomManager();
+	public static getRoomManager(): RoomManager {
+		return this.roomManager;
+	}
 
+	public rooms: Room[] = [];
 	private constructor() {}
 
 	findByRoomName(roomName: string): Room | undefined {
 		return this.rooms.find(room => room.roomName == roomName);
-	}
-	findByRoomNameToIndex(roomName: string): number {
-		return this.rooms.findIndex(room => room.roomName == roomName);
 	}
 	joinRoom(roomName: string, socket: SocketIO.Socket): void {
 		let room = this.findByRoomName(roomName);
@@ -40,17 +54,37 @@ class RoomManager {
 			room.joinUser(socket.id);
 			this.rooms.push(room);
 		}
+		socket.join(roomName);
 	}
-	leaveRoom(roomName: string, socket: SocketIO.Socket): void {
-		let roomIdx = this.findByRoomNameToIndex(roomName);
+	leaveRoom(socket: SocketIO.Socket): void {
+		let roomIdx = this.rooms.findIndex(room => room.users.has(socket.id));
 		if (roomIdx != -1) {
 			let room = this.rooms[roomIdx];
 			if (room.leaveUser(socket.id)) {
 				this.rooms.splice(roomIdx, 1);
 			}
 		}
+		socket.leaveAll();
 	}
 }
 
-const socketRouter: SocketRouter = (io: SocketIO.Server, socket: SocketIO.Socket): void => {};
+const roomManager = RoomManager.getRoomManager();
+
+const socketRouter: SocketRouter = (io: SocketIO.Server, socket: SocketIO.Socket): void => {
+	socket.on("game_joinRoom", (data: JoinRoomRequest) => {
+		roomManager.joinRoom(data.roomName, socket);
+		socket.emit("game_joinRoom", roomManager.findByRoomName(data.roomName));
+	});
+	socket.on("game_propCreate", (data: PropRequest) => {
+		roomManager.findByRoomName(data.roomName).addProp(data.prop);
+		io.sockets.in(data.roomName).emit("game_propCreate", data.prop);
+	});
+	socket.on("game_propUpdate", (data: PropRequest) => {
+		roomManager.findByRoomName(data.roomName).updateProp(data.prop);
+		socket.broadcast.in(data.roomName).emit("game_propUpdate", data.prop);
+	});
+	socket.on("disconnect", () => {
+		roomManager.leaveRoom(socket);
+	});
+};
 export default socketRouter;
--- src/routers/socket.index.ts
+++ src/routers/socket.index.ts
@@ -1,6 +1,8 @@
 import { Server } from "http";
 import * as SocketIO from "socket.io";
 
+import GameRouter from "./game/socket.game";
+
 export type SocketRouter = (io: SocketIO.Server, socket: SocketIO.Socket) => void;
 
 class SocketIOManager {
@@ -15,10 +17,12 @@ class SocketIOManager {
 	start(server: Server) {
 		this.io = SocketIO(server, { origins: "*:*" });
 		this.io.on("connection", socket => {
+			console.log("CONNECT");
 			this.socketRouters.forEach(socketRouter => socketRouter(this.io, socket));
 		});
 	}
 }
 const socketIOManager = new SocketIOManager();
+socketIOManager.use(GameRouter);
 
 export default socketIOManager;
